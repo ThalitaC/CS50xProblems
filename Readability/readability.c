20a17482003721571238de7bc891501840de5712
ThalitaC <70970971+ThalitaC@users.noreply.github.com>
2020-12-02 12:36:33 -0300

    Update readability.c

--- Readability/readability.c
+++ Readability/readability.c
@@ -36,26 +36,26 @@ int main(void)
         {
             csentences++;
         }
-    
-    
-        //coleman-liau index = 0.0588 * L - 0.296 * S - 15.8
-        double  l = ((double) cletters / cwords) * 100;
-        double  s = ((double) csentences / cwords) * 100;
-        double  index = 0.0588 * l - 0.296 * s - 15.8;
-    
-        if (index < 0)
-        {
-            printf("Before Grade 1\n");
-        }
-    
-        else if (index > 16)
-        {
-            printf("Grade 16+\n");
-        }
-    
-        else
-        {
-            printf("Grade %.0f\n", round(index));
-        }
     }
-
+    
+    //coleman-liau index = 0.0588 * L - 0.296 * S - 15.8
+    double  l = ((double) cletters / cwords) * 100;
+    double  s = ((double) csentences / cwords) * 100;
+    double  index = 0.0588 * l - 0.296 * s - 15.8;
+    
+    if (index < 0)
+    {
+        printf("Before Grade 1\n");
+    }
+    
+    else if (index > 16)
+    {
+        printf("Grade 16+\n");
+    }
+    
+    else
+    {
+        printf("Grade %.0f\n", round(index));
+    }
+    
+}
