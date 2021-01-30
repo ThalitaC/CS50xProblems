4157f3a9947cbe69867d6f6924e76d0bb0275a1e
Thalita Cecilier <70970971+ThalitaC@users.noreply.github.com>
2021-01-30 16:46:24 -0300

    Create plurality.c

---
+++ Plurality/plurality.c
@@ -0,0 +1,111 @@
+////DISTRIBUTION CODE
+
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// Max number of candidates
+#define MAX 9
+
+// Candidates have name and vote count
+typedef struct
+{
+    string name;
+    int votes;
+}
+candidate;
+
+// Array of candidates
+candidate candidates[MAX];
+
+// Number of candidates
+int candidate_count;
+
+// Function prototypes
+bool vote(string name);
+void print_winner(void);
+
+int main(int argc, string argv[])
+{
+    // Check for invalid usage
+    if (argc < 2)
+    {
+        printf("Usage: plurality [candidate ...]\n");
+        return 1;
+    }
+
+    // Populate array of candidates
+    candidate_count = argc - 1;
+    if (candidate_count > MAX)
+    {
+        printf("Maximum number of candidates is %i\n", MAX);
+        return 2;
+    }
+    for (int i = 0; i < candidate_count; i++)
+    {
+        candidates[i].name = argv[i + 1];
+        candidates[i].votes = 0;
+    }
+
+    int voter_count = get_int("Number of voters: ");
+
+    // Loop over all voters
+    for (int i = 0; i < voter_count; i++)
+    {
+        string name = get_string("Vote: ");
+
+        // Check for invalid vote
+        if (!vote(name))
+        {
+            printf("Invalid vote.\n");
+        }
+    }
+
+    // Display winner of election
+    print_winner();
+}
+////END OF DISTRIBUTION CODE
+
+// Update vote totals given a new vote
+bool vote(string name)
+{
+    //iterate candidates_count
+    for (int i = 0; i <  candidate_count; i++)
+    {
+        //compares candidate names and votes and increment votes
+        if (strcmp(name, candidates[i].name) == 0)
+        {
+            candidates[i].votes++;
+            return true;
+        }
+    }
+    //if strings don't match return false
+    return false;
+}
+
+// Print the winner (or winners) of the election
+void print_winner(void)
+{
+    int mostvoted = 0;
+    
+    //check candidate_count
+    for (int i = 0; i < candidate_count; i++)
+    {
+        //increment most voted
+        if (candidates[i].votes > mostvoted)
+        {
+            mostvoted = candidates[i].votes;
+        }
+    }
+
+    //print most voted name(s)
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes ==  mostvoted)
+        {
+            printf("%s\n", candidates[i].name);
+            
+        }
+    }
+    return;
+}
